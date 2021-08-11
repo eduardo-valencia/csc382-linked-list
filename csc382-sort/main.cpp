0b8560a8b9422fe0aaaaebf7ffb8f86b8c5e72d7
EDUARDO VALENCIA <8025918@gmail.com>
2021-08-11 14:27:09 -0700

    Link main project with linked list

---
+++ csc382-sort/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../csc382-linked-list-test/LinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world\n";
+	// LinkedList<int> myLinkedList{ 0 };
+	// myLinkedList.Insert(100);
+	return 0;
+}
